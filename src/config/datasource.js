cdc57de702c31b48fbcfeda73702701b4391e1f6
thismarcoantonio <thismarcoantonio@gmail.com>
2019-05-06 02:42:25 -0300

    Update process

--- src/config/datasource.js
+++ src/config/datasource.js
@@ -1,7 +1,11 @@
 import { connect, connection } from "mongoose"
 
 function datasource() {
-  connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+  connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    user: process.env.DATABASE_USER,
+    pass: process.env.DATABASE_PASSWORD
+  })
   connection.once("open", () => {
     console.info("Connected at port 27701")
   })
